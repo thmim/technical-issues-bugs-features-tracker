f08fccc0d0af61229530620a474b6baeb37dea47
thmim <mdtaharimmim1010@gmail.com>
2026-06-11 20:50:43 +0600

    connected with database

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,40 @@
 import express, { type Application, type Request, type Response } from "express";
-const app:Application = express();
+import { Pool } from "pg";
+const app: Application = express();
 const port = 3000
 app.use(express.json());
 
-app.get('/', (req:Request, res:Response) => {
-  res.status(200).json({
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_MRIHZQFanU90@ep-green-night-atkuypps-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
+const initDb = async () => {
+    try {
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        role VARCHAR(20) DEFAULT 'contributer',
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+        console.log("connected sucessfully");
+    } catch (error) {
+        console.log(error);
+    }
+}
+initDb();
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
         message: "choltece",
         author: "ami"
     })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
