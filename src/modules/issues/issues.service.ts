918d5f7302db21858ad8a758f4db159aa9ec7239
thmim <mdtaharimmim1010@gmail.com>
2026-06-13 14:03:57 +0600

    added sorting method in the all issues getting api

--- src/modules/issues/issues.service.ts
+++ src/modules/issues/issues.service.ts
@@ -12,38 +12,47 @@ const createIssuesIntoDb = async (payload: IIssues) => {
 }
 
 // get all issues from db
-const getAllIssuesFromDb = async () => {
+const getAllIssuesFromDb = async (sort: string) => {
+    // decide sorting order
+    let order = "DESC"; // newest first default
+
+    if (sort === "oldest") {
+        order = "ASC";
+    }
+
+
     const result = await pool.query(`
             SELECT * FROM issues
+            ORDER BY created_at ${order}
             `);
     // return result;
     const reporterIds = result.rows.map(i => i.reporter_id);
     // console.log(reporterIds)
 
-// users getting query
+    // users getting query
     const usersInfo = await pool.query(`
   SELECT id, name, role
   FROM users
   WHERE id = ANY($1)
 `, [reporterIds]);
-// console.log(usersInfo)
-const userMap = new Map();
-for (const user of usersInfo.rows) {
-  userMap.set(user.id, user);
-}
-// console.log(userMap)
-const data = result.rows.map(issue => ({
-  id: issue.id,
-  title: issue.title,
-  description:issue.description,
-  type:issue.type,
-  status:issue.status,
-  reporter: userMap.get(issue.reporter_id),
-  created_at:issue.created_at,
-  updated_at:issue.updated_at
-}));
-// console.log(data)
-return data;
+    // console.log(usersInfo)
+    const userMap = new Map();
+    for (const user of usersInfo.rows) {
+        userMap.set(user.id, user);
+    }
+    // console.log(userMap)
+    const data = result.rows.map(issue => ({
+        id: issue.id,
+        title: issue.title,
+        description: issue.description,
+        type: issue.type,
+        status: issue.status,
+        reporter: userMap.get(issue.reporter_id),
+        created_at: issue.created_at,
+        updated_at: issue.updated_at
+    }));
+    // console.log(data)
+    return data;
 }
 
 export const issuesService = {
